f2e6220695e68c2215b2b786ad5440e2c9e3549a
Yuuki Noda <ynoda@eiffelcard.com>
2017-06-14 19:48:09 +0900

    SPAのバージョンとわけた

--- ver2_simplebiz/src/App.js
+++ ver2_simplebiz/src/App.js
@@ -1,45 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import Register from './Register';
-import Mainmenu from './Mainmenu';
-import Message from './Message';
-import Order from './Order';
-import Address from './Address';
 
 class App extends Component {
-   constructor(props){
-    super(props);
-     this.state = {
-      currentPage:'Address'
-     };
-    this.movepage=this.movepage.bind(this);
-   }
-
-movepage(name){
-  this.setState({
-    currentPage:name
-  });
-}
-
  render() {
-    const pageComponent={
-      Address:Address,
-      Home:Home
-    }
-
-
-    const Page=pageComponent[this.state.currentPage]
-
-
     return (
       <div className="App">
-         <Page/>
-         <p>hello</p>
-         <button onClick={()=>{this.movepage('Home')}}>Change!</button>
+            <p>{this.props.children}</p>
+        
       </div>
-
     );
   }
 }
--- ver2_simplebiz/src/index.js
+++ ver2_simplebiz/src/index.js
@@ -4,12 +4,25 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-
+import Home from './Home';
+import Register from './Register';
+import Mainmenu from './Mainmenu';
+import Message from './Message';
+import Order from './Order';
+import Address from './Address';
 
 
 
 ReactDOM.render(
+  <BrowserRouter>
     <App>
-    </App>,
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/register" component={Register}/>
+     <Route exact path="/mainmenu" component={Mainmenu}/>
+     <Route exact path="/Message" component={Message}/>
+     <Route exact path="/Order" component={Order}/>
+     <Route exact path="/Address" component={Address}/>
+    </App>
+  </BrowserRouter>,
   document.getElementById('root')
 );
