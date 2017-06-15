cf99428604a8dd07acfb165908518a138214f8bc
Yuuki Noda <ynoda@eiffelcard.com>
2017-06-15 13:08:24 +0900

    修正

--- ver2_simplebiz/src/Register.js
+++ ver2_simplebiz/src/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import request from 'superagent';
+
 import { Link } from 'react-router-dom';
 import RegisterForm from './RegisterForm';
 
